8b458b97a3414bb10e7082b75819209a965926fb
unknown <samuel.gomes.p99@gmail.com>
2025-04-16 07:42:17 -0300

    Pasta views removida, projeto não terá interface

--- src/server.js
+++ src/server.js
@@ -8,11 +8,6 @@ app.use(express.json())
 
 app.use('/home', mainRoute)
 
-
-app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, '/views'))
-app.use(express.urlencoded({ extended: true }))
-
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => console.log(`Servidor inciado em <http://localhost>:${PORT}/`))
