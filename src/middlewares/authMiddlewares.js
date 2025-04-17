a60180f2b52731f3c0aa8594f67cc03b58454e33
unknown <samuel.gomes.p99@gmail.com>
2025-04-17 16:43:13 -0300

    Ultimas funcionalidade adicionadas, rotas de admin criadas, rotas para atualizar usuários, mostrar e excluir, projeto finalizado

--- src/middlewares/authMiddlewares.js
+++ src/middlewares/authMiddlewares.js
@@ -25,7 +25,30 @@ const middlewareDashboard = (req, res, next) => {
 }
 
 const authMiddleware = (req, res, next) => {
+    const authHeader = req.headers.authorization
 
+    if (!authHeader) {
+        return res.status(401).json({ message: 'Token not provided' })
+    }
+
+    const token = authHeader.split(' ')[1]
+
+    try {
+        const decodedToken = jwt.verify(token, secretKey)
+
+        const user = users.find(user => user.username === decodedToken.username)
+
+        if (user.role !== 'admin') {
+            return res.status(401).json({ message: 'Access denied' })
+        }
+
+        req.authenticatedUser = user
+
+        next()
+
+    } catch (error) {
+        return res.status(401).json({ message: 'Invalid token' })
+    }
 }
 
 module.exports = { authMiddleware, middlewareDashboard }
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,7 +1,12 @@
 const express = require('express')
-const { middlewareDashboard } = require('../middlewares/authMiddlewares')
+const { middlewareDashboard, authMiddleware } = require('../middlewares/authMiddlewares')
+const users = require('../models/users')
 
 const authRouter = express.Router()
+
+function findUser(email) {
+    return users.find(user => user.email === email)
+}
 
 authRouter.get('/dashboard', middlewareDashboard, (req, res) => {
 
@@ -11,6 +16,61 @@ authRouter.get('/dashboard', middlewareDashboard, (req, res) => {
 
     res.status(200).json({ message: `Welcome to the dashboard ${req.authenticatedUser.username}!` })
 
+})
+
+authRouter.get('/dashboard/admin', authMiddleware, (req, res) => {
+    res.status(200).json({ message: `Welcome to the admin dashboard ${req.authenticatedUser.username}!` })
+})
+
+authRouter.get('/dashboard/admin/show', authMiddleware, (req, res) => {
+    const { username, email } = req.body
+
+    const user = findUser(email)
+
+    if (!user) {
+        return res.status(404).json({ message: 'User not found' })
+    }
+
+    res.status(200).json({ message: `User ${username} found!`, user })
+})
+
+authRouter.post('/dashboard/admin/update', authMiddleware, (req, res) => {
+    const { email, newName, newEmail, newRole } = req.body
+
+    const user = findUser(email)
+
+    if (!user) {
+        return res.status(404).json({ message: 'User not found' })
+    }
+
+    if (newName) {
+        user.username = newName
+    }
+
+    if (newEmail) {
+        user.email = newEmail
+    }
+
+    if (newRole) {
+        user.role = newRole
+    }
+
+    res.status(201).json({ message: `User ${user.username} updated!`, user })
+
+})
+
+authRouter.delete('/dashboard/admin/:email', authMiddleware, (req, res) => {
+    const { email } = req.params
+
+    const userIndex = users.findIndex(user => user.email === email)
+
+    if (userIndex === -1) {
+        res.status(404).json({ message: "User not Found!" })
+    }
+
+    users.splice(userIndex, 1)
+
+    res.status(204).end()
 })
 
 module.exports = authRouter
